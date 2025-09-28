4cf3ba632300ec2d9ec0f38c38d3b9606b8171b7
BlueCloudK <thanhkiennk@gmail.com>
2025-09-28 12:07:35 +0700

    Chưa xong db

---
+++ BE/oem-ev-warranty-management-system/src/main/java/com/swp391/warrantymanagement/controller/RoleController.java
@@ -0,0 +1,48 @@
+package com.swp391.warrantymanagement.controller;
+
+import com.swp391.warrantymanagement.entity.Role;
+import com.swp391.warrantymanagement.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/roles")
+@CrossOrigin
+public class RoleController {
+    @Autowired
+    private RoleService roleService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Role>> getRoles() {
+        return ResponseEntity.ok(roleService.getRoles());
+    }
+
+    @GetMapping("/roleId")
+    public ResponseEntity<Role> getRoleById(@RequestParam int roleId) {
+        Role role = roleService.getById(roleId);
+        if (role != null) {
+            return ResponseEntity.ok(role);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Role> createRole(@RequestBody Role role) {
+        return ResponseEntity.ok(roleService.createRole(role));
+    }
+
+    @DeleteMapping("/roleId")
+    public ResponseEntity<Void> deleteRole(@PathVariable int roleId) {
+        roleService.deleteRole(roleId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Role> updateRole(@RequestBody Role role) {
+        return ResponseEntity.ok(roleService.updateRole(role));
+    }
+}
---
+++ BE/oem-ev-warranty-management-system/src/main/java/com/swp391/warrantymanagement/service/RoleService.java
@@ -0,0 +1,17 @@
+package com.swp391.warrantymanagement.service;
+
+import com.swp391.warrantymanagement.entity.Role;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface RoleService {
+    public Role findByRoleName(String roleName);
+    public Role getById(int id);
+    public Role createRole(Role role);
+    public Role updateRole(Role role);
+    public void deleteRole(int id);
+
+    public List<Role> getRoles();
+}
