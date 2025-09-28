914ee6b32db440a6cd1cc3b32a5ccaf2954ab4de
BlueCloudK <thanhkiennk@gmail.com>
2025-09-28 12:07:35 +0700

    Chưa xong db

---
+++ BE/oem-ev-warranty-management-system/src/main/java/com/swp391/warrantymanagement/service/RoleServiceImpl.java
@@ -0,0 +1,50 @@
+package com.swp391.warrantymanagement.service;
+
+import com.swp391.warrantymanagement.entity.Role;
+import com.swp391.warrantymanagement.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public Role findByRoleName(String roleName) {
+        return roleRepository.findByRoleName(roleName);
+    }
+
+    @Override
+    public Role getById(int id) {
+        return roleRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Role createRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role updateRole(Role role) {
+        Role existingRole = roleRepository.findById(role.getRoleId()).orElse(null);
+        if (existingRole != null) {
+            existingRole.setRoleName(role.getRoleName());
+            return roleRepository.save(existingRole);
+        }
+        return existingRole;
+    }
+
+    @Override
+    public void deleteRole(int id) {
+        roleRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Role> getRoles() {
+        return roleRepository.findAll();
+    }
+}
