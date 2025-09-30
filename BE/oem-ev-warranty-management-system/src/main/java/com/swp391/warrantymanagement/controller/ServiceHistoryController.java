ecd1dbc88f17752902120c0806b62dbce49548ca
BlueCloudK <thanhkiennk@gmail.com>
2025-09-30 12:08:43 +0700

    Delete extra code

--- BE/oem-ev-warranty-management-system/src/main/java/com/swp391/warrantymanagement/controller/ServiceHistoryController.java
+++ BE/oem-ev-warranty-management-system/src/main/java/com/swp391/warrantymanagement/controller/ServiceHistoryController.java
@@ -15,34 +15,5 @@ public class ServiceHistoryController {
     @Autowired
     private ServiceHistoryService serviceHistoryService;
 
-    @GetMapping("/")
-    public ResponseEntity<List<ServiceHistory>> getServiceHistories() {
-        return ResponseEntity.ok(serviceHistoryService.getServiceHistories());
-    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ServiceHistory> getServiceHistoryById(@PathVariable Long id) {
-        ServiceHistory serviceHistory = serviceHistoryService.getById(id);
-        if (serviceHistory != null) {
-            return ResponseEntity.ok(serviceHistory);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<ServiceHistory> createServiceHistory(@RequestBody ServiceHistory serviceHistory) {
-        return ResponseEntity.ok(serviceHistoryService.createServiceHistory(serviceHistory));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteServiceHistory(@PathVariable Long id) {
-        serviceHistoryService.deleteServiceHistory(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PutMapping("/")
-    public ResponseEntity<ServiceHistory> updateServiceHistory(@RequestBody ServiceHistory serviceHistory) {
-        return ResponseEntity.ok(serviceHistoryService.updateServiceHistory(serviceHistory));
-    }
 }
