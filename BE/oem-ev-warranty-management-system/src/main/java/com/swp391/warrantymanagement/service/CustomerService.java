2c4343f0638164ffd264a6a1ee8e1c4847d0b216
BlueCloudK <thanhkiennk@gmail.com>
2025-09-30 15:59:15 +0700

    Config UUID to add Data to DataBase

--- BE/oem-ev-warranty-management-system/src/main/java/com/swp391/warrantymanagement/service/CustomerService.java
+++ BE/oem-ev-warranty-management-system/src/main/java/com/swp391/warrantymanagement/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.swp391.warrantymanagement.service;
 import com.swp391.warrantymanagement.entity.Customer;
 import java.util.List;
 import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 // class đứng giữa Controller và Repository, nó sẽ đưa Object từ Repository lên Controller và ngược lại
