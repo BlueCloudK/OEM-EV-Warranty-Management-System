2f4e87ae52db45f44e79c43d773d2720fe9e6e08
BlueCloudK <thanhkiennk@gmail.com>
2025-11-01 10:08:25 +0700

    Test Src

---
+++ BE/oem-ev-warranty-management-system/src/test/java/com/swp391/warrantymanagement/OemEvWarrantyManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.swp391.warrantymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class
+OemEvWarrantyManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
